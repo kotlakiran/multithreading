4ac23a2163eb89b915fa4448ea58dc96d78c7f6b
kiran <kirankumar_reddykotla@intuit.com>
2019-01-07 15:01:28 +0530

    Thread example

---
+++ MultithreadingBasics/src/main/java/com/kiran/multithreading/ThreadExample.java
@@ -0,0 +1,39 @@
+package com.kiran.multithreading;
+
+
+
+
+
+class Thread1 extends Thread{
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Thread1: "+i);
+		}
+	}
+}
+
+class Thread2 extends Thread {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Thread2: "+i);
+		}
+	}
+}
+
+public class ThreadExample {
+	
+	public static void main(String[] args) {
+		
+		Thread1 t1=new Thread1();
+		Thread2 t2=new Thread2();
+		t1.start();
+		t2.start();
+		 
+	}
+	
+}
+
